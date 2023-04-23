965f1927f6d7cb5bab5f8dd9dd308d2b6014fc2d
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 02:07:42 +0300

    Styled HTML section tag with TailwindCSS's class mx-auto

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <PageLayout title='Home | MemoryDiary'>
       <main className="w-full h-screen flex flex-col lg:flex-row lg:justify-around gap-8 text-center p-4">
-        <section class="max-w-screen-sm ">
+        <section class="max-w-screen-sm mx-auto ">
 
         </section>
       </main>
