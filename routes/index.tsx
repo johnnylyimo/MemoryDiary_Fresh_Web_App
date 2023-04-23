d85eed53cac38ed9c1c5c6db1afe883c8f9137a0
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 02:07:22 +0300

    Styled HTML section tag with TailwindCSS's class max-w-screen

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <PageLayout title='Home | MemoryDiary'>
       <main className="w-full h-screen flex flex-col lg:flex-row lg:justify-around gap-8 text-center p-4">
-        <section>
-          
+        <section class="max-w-screen-sm ">
+
         </section>
       </main>
 
