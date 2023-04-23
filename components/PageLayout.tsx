9a1a4546526ccfd4e3c84c368d33563bceec71b0
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 01:34:36 +0300

    Syled HTML main tag wih TailwindCSS's class h-full

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -7,7 +7,7 @@ export default function PageLayout({children, title}:{children:any, title:string
                 <title>{title}</title>
                 <meta name="description" content="MemoryDiary's home page" />
             </Head>
-            <main class='w-full'> 
+            <main class='w-full h-full'> 
 
             </main>
         </>
