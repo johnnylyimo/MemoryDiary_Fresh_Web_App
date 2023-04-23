7b476c606ff6ff282bb72ff658c6cd3afd9355bd
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 02:16:38 +0300

    Styled HTML paragraph tag with TailwindCSS's class text-xl

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           <h1 class="text-2xl font-bold uppercase pt-4 tracking-wide">
             Word of the day
           </h1>
-          <p>
+          <p class='text-xl'>
             Today will be better than yesterday
           </p>
         </section>
