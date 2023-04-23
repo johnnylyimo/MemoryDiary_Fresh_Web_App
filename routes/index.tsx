b17ddbad3433d00577a4a9dd711448c8871a41fb
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 01:42:27 +0300

    Added HTML main tag

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,9 @@ import PageLayout  from "../components/PageLayout.tsx";
 export default function Home() {
   return (
     <PageLayout title='Home | MemoryDiary'>
-      <h1>Hello Folks</h1>
+      <main>
+        
+      </main>
     </PageLayout>
   );
 }
