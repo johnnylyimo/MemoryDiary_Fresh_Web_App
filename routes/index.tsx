413211767229a9e371da033c157622b1888b19df
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 02:02:32 +0300

    Styled HTML main tag with TailwindCSS's class flex

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,7 @@ import PageLayout  from "../components/PageLayout.tsx";
 export default function Home() {
   return (
     <PageLayout title='Home | MemoryDiary'>
-      <main className="w-full h-screen  ">
+      <main className="w-full h-screen flex ">
       </main>
 
     </PageLayout>
