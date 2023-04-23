b1e696f9d4930553008b93dcb75a55379e76c728
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 02:05:03 +0300

    Styled HTML main tag with TailwindCSS's class text-center

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,7 @@ import PageLayout  from "../components/PageLayout.tsx";
 export default function Home() {
   return (
     <PageLayout title='Home | MemoryDiary'>
-      <main className="w-full h-screen flex flex-col lg:flex-row lg:justify-around gap-8 ">
+      <main className="w-full h-screen flex flex-col lg:flex-row lg:justify-around gap-8 text-center ">
       </main>
 
     </PageLayout>
