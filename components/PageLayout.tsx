2bd205b9dbaf9114c2a57aa59ec5caacb5fab89c
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 01:25:28 +0300

    Defined Head tag

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -3,6 +3,9 @@ import { Head } from "$fresh/runtime.ts";
 export default function PageLayout({children, title}:{children:any, title:string}) {
     return (
         <>
+            <Head>
+                
+            </Head>
         </>
     )
 }
