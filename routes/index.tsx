ccf41fbb6dc691047cedec5e7422df13ee023e8d
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 02:00:49 +0300

    Styled HTML main tag with TailwindCSS's class w-full

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,9 +3,9 @@ import PageLayout  from "../components/PageLayout.tsx";
 export default function Home() {
   return (
     <PageLayout title='Home | MemoryDiary'>
-      <main>
-        
+      <main className="w-full   ">
       </main>
+
     </PageLayout>
   );
 }
