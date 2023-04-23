f771c9f960b2adb154c02a39d37ab4f3c6cf770a
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 02:09:19 +0300

    Styled HTML section tag with TailwindCSS's class bg color

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <PageLayout title='Home | MemoryDiary'>
       <main className="w-full h-screen flex flex-col lg:flex-row lg:justify-around gap-8 text-center p-4">
-        <section class="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 ">
+        <section class="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 bg-[#ce93d8] ">
 
         </section>
       </main>
