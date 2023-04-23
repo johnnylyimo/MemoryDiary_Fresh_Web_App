78912b64efec433c5a7d302ac0d63879f458e614
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 01:36:02 +0300

    Defined children placeholder within HTML main tag

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -8,7 +8,7 @@ export default function PageLayout({children, title}:{children:any, title:string
                 <meta name="description" content="MemoryDiary's home page" />
             </Head>
             <main class='w-full h-full bg-[#E1BEE7]'> 
-
+             {children}
             </main>
         </>
     )
