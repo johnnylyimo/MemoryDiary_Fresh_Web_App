6e8bfcc12f4154dafcf409466005d1323811b241
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 04:07:52 +0300

    Renamed component name

--- islands/CreateDisplayMemory.tsx
+++ islands/CreateDisplayMemory.tsx
@@ -1,5 +1,5 @@
 
-export default function Home() {
+export default function CreateDisplayMemory() {
   return (
       <div className="w-full h-screen flex flex-col lg:flex-row lg:justify-around gap-8 text-center p-4">
         <section class="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 bg-[#ce93d8] self-start">
