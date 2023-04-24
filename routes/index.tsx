be1f48ee7a14d41fe5d9860e0b9e289f0fc584d1
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 04:08:59 +0300

    Defined new Component Home

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,6 +3,10 @@ import CreateDisplayMemory from "../islands/CreateDisplayMemory.tsx";
 
 <PageLayout title='Home | MemoryDiary'>
     <main>
-        
+        <CreateDisplayMemory />
     </main>
 </PageLayout>
+
+export default function Home() {
+ 
+}
