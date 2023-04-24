5663ec04d9810d0dbd50e96efb5aec7723b7ce7f
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 04:09:52 +0300

    Defined function return

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,12 +1,13 @@
 import PageLayout  from "../components/PageLayout.tsx";
 import CreateDisplayMemory from "../islands/CreateDisplayMemory.tsx";
 
-<PageLayout title='Home | MemoryDiary'>
-    <main>
-        <CreateDisplayMemory />
-    </main>
-</PageLayout>
 
 export default function Home() {
- 
+  return (
+    <PageLayout title='Home | MemoryDiary'>
+        <main>
+            <CreateDisplayMemory />
+        </main>
+    </PageLayout>
+  )
 }
