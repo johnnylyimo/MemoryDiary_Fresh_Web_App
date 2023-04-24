4f9cb5be0d4837420584fbafb25b10dde823b9a0
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 03:33:03 +0300

    Moved file to island directory

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,14 +4,15 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/api/joke.ts";
-import * as $1 from "./routes/index.tsx";
+import * as $$0 from "./islands/CreateDisplayMemory.tsx";
 
 const manifest = {
   routes: {
     "./routes/api/joke.ts": $0,
-    "./routes/index.tsx": $1,
   },
-  islands: {},
+  islands: {
+    "./islands/CreateDisplayMemory.tsx": $$0,
+  },
   baseUrl: import.meta.url,
   config,
 };
