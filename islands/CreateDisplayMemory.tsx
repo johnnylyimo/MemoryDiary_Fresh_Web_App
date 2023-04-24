bfb11a7302f9211df18163ed7206b5d0b7942ba7
johnnylyimo <johnnylyimo@gmail.com>
2023-04-24 03:38:38 +0300

    Refactored codes on CreateDisplayMemory

--- islands/CreateDisplayMemory.tsx
+++ islands/CreateDisplayMemory.tsx
@@ -1,9 +1,7 @@
-import PageLayout  from "../components/PageLayout.tsx";
 
 export default function Home() {
   return (
-    <PageLayout title='Home | MemoryDiary'>
-      <main className="w-full h-screen flex flex-col lg:flex-row lg:justify-around gap-8 text-center p-4">
+      <div className="w-full h-screen flex flex-col lg:flex-row lg:justify-around gap-8 text-center p-4">
         <section class="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 bg-[#ce93d8] self-start">
           <h1 class="text-2xl font-bold uppercase pt-4 tracking-wide">
             Word of the day
@@ -15,8 +13,6 @@ export default function Home() {
             Anytime something positive happens, make a note of it and come back to it later.
           </p>
         </section>
-      </main>
-
-    </PageLayout>
+      </div>
   );
 }
