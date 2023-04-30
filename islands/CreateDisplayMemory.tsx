544647872a0d94918f6048a90105538150192efd
molystechie <molystechie@gmail.com>
2023-04-30 16:35:08 +0300

    Styled HTML label tag with TailwindCSS's class text-color

--- islands/CreateDisplayMemory.tsx
+++ islands/CreateDisplayMemory.tsx
@@ -13,7 +13,7 @@ export default function CreateDisplayMemory() {
             Anytime something positive happens, make a note of it and come back to it later.
           </p>
           <form class='p-2 mt-6'>
-            <label class='block text-xl'>
+            <label class='block text(md gray-900)'>
               Write new memory below
             </label>
 
