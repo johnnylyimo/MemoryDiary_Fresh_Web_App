ab5f0d24d03cc11e1eb0151faa40a13e088cdd5c
molystechie <molystechie@gmail.com>
2023-04-30 16:38:27 +0300

    Styled HTML label tag with TailwindCSS's class mb-2

--- islands/CreateDisplayMemory.tsx
+++ islands/CreateDisplayMemory.tsx
@@ -13,7 +13,7 @@ export default function CreateDisplayMemory() {
             Anytime something positive happens, make a note of it and come back to it later.
           </p>
           <form class='p-2 mt-6'>
-            <label class='block text(md gray-900) font-medium'>
+            <label class='block text(md gray-900) font-medium mb-2'>
               Write new memory below
             </label>
 
