eeef3b938eb21e6feb3457f3881dd51846000dd0
johnnylyimo <johnnylyimo@gmail.com>
2023-05-01 05:35:52 +0300

    Created prisma object

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,3 +2,7 @@ import {Prisma, PrismaClient} from '../generated/client/deno/edge.ts'
 import { load } from "https://deno.land/std/dotenv/mod.ts";
 
 const env = await load();
+
+const prisma = new PrismaClient({
+    
+})
