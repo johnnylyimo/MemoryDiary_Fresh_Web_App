97b73f577177573f1d605e375e2e42771fd77d3f
johnnylyimo <johnnylyimo@gmail.com>
2023-05-01 04:54:27 +0300

    Codes refactored

--- islands/CreateDisplayMemory.tsx
+++ islands/CreateDisplayMemory.tsx
@@ -7,7 +7,7 @@ export default function CreateDisplayMemory() {
   });
 
   const [editMemoryPopUp, setEditMemoryPopUp]=useState(false);
-  
+
   return (
       <div className="w-full h-screen flex flex-col lg:flex-row lg:justify-around gap-8 text-center p-4">
         <section class="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 bg-[#ce93d8] self-start">
