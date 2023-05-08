098ace7a8f22f807799ee20b59a74b06002eae71
johnnylyimo <johnnylyimo@gmail.com>
2023-05-08 03:56:12 +0300

    Styled HTML header tag with TailwindCSS's class text size

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 
 export default function Header() {
   return (
-    <header class='flex justify-center pt-5'>
+    <header class='flex justify-center pt-5 text-4xl '>
         <a href="/">Home</a>
     </header>
   )
