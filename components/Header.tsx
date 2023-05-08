86440ad3cd1a448c634c708fcbeedb3b6341f165
johnnylyimo <johnnylyimo@gmail.com>
2023-05-08 03:57:39 +0300

    Styled HTML header tag with TailwindCSS's class margin bottom

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 
 export default function Header() {
   return (
-    <header class='flex justify-center pt-5 text-4xl font-bold text-[#ac5eb9dc] underline hover:text-[#800080]'>
+    <header class='flex justify-center pt-5 text-4xl font-bold text-[#ac5eb9dc] underline hover:text-[#800080] mb-12'>
         <a href="/">Home</a>
     </header>
   )
