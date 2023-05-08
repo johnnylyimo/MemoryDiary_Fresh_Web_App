b66efc263ef78c1fb5f0037324d4a25a12a2273e
johnnylyimo <johnnylyimo@gmail.com>
2023-05-08 03:56:46 +0300

    Styled HTML header tag with TailwindCSS's class text-color

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 
 export default function Header() {
   return (
-    <header class='flex justify-center pt-5 text-4xl font-bold '>
+    <header class='flex justify-center pt-5 text-4xl font-bold text-[#ac5eb9dc]'>
         <a href="/">Home</a>
     </header>
   )
