0278a4fa916822522d0fc8e3bef440925e199c3f
johnnylyimo <johnnylyimo@gmail.com>
2023-05-08 04:54:44 +0300

    Created function component, named Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,10 @@
 import IconMail from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/mail.tsx"
 import IconPhone from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/phone.tsx"
 import IconMapPinFilled from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/map-pin-filled.tsx"
+
+
+export default function Footer() {
+  return (
+    
+  )
+}
