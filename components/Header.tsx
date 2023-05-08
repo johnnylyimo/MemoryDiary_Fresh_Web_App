7c00bf4e7a5a073fdf4dcc0b63f44e64d94b80ca
johnnylyimo <johnnylyimo@gmail.com>
2023-05-08 03:57:03 +0300

    Styled HTML header tag with TailwindCSS's class underline

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 
 export default function Header() {
   return (
-    <header class='flex justify-center pt-5 text-4xl font-bold text-[#ac5eb9dc]'>
+    <header class='flex justify-center pt-5 text-4xl font-bold text-[#ac5eb9dc] underline '>
         <a href="/">Home</a>
     </header>
   )
