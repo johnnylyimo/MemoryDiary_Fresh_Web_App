11283560852b1eddd1f5fc367784c424ec0f20cf
johnnylyimo <johnnylyimo@gmail.com>
2023-06-26 02:41:29 +0300

    Styled HTML footer tag with TailwindCSS's grid gap

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import IconMapPinFilled from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/map
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer class='w-full mb-0 p-4 mt-20 bg-[#ce93d8] grid md:grid-cols-5 '>
+    <footer class='w-full mb-0 p-4 mt-20 bg-[#ce93d8] grid md:grid-cols-5 gap-4'>
     </footer>
   )
 }
