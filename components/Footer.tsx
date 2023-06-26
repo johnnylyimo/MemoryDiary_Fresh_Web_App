51f0648c67092fd93e93b257b02e07b8a3f275ce
johnnylyimo <johnnylyimo@gmail.com>
2023-06-26 03:42:15 +0300

    Styled HTML icon component height attribute

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -37,7 +37,7 @@ export default function Footer() {
               Find Me
             </h1>
             <div className="flex justify-center gap-x-1">
-              <IconMapPinFilled  />
+              <IconMapPinFilled class="h-6" />
                 
             </div>
 
