abdfacf15b36edbf93c3116a37c7659d54c45f2e
johnnylyimo <johnnylyimo@gmail.com>
2023-06-26 03:31:18 +0300

    Styled HTML div tag with TailwindCSS's class flex gap

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ export default function Footer() {
               </a>
             </div>
           </div>
-          <div className="flex flex-col ">
+          <div className="flex flex-col gap-2">
 
           </div>
 
