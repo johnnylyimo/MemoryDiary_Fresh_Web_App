a5629e6a3a707ba09709e06856099683bd1ea784
johnnylyimo <johnnylyimo@gmail.com>
2023-06-26 03:04:14 +0300

    Styled HTML h1 tag with TailwindCSS's class text-size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,8 +17,8 @@ export default function Footer() {
       <section class='grid md:col-span-3 gap-6 text-center'>
         <div className="grid gap-y-6 md:flex md:justify-around">
           <div className="flex flex-col gap-2">
-            <h1>
-              
+            <h1 class="text-lg ">
+
             </h1>
 
           </div>
