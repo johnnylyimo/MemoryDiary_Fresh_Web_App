269a4746cba4008ade5f27fcaaed33051946c037
johnnylyimo <johnnylyimo@gmail.com>
2023-06-26 03:56:13 +0300

    Added HTML paragraph tag

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -49,6 +49,7 @@ export default function Footer() {
             </h1>
             <div class="flex justify-center gap-x-1">
               <IconPhone class="w-6 h-6" />
+              <p></p>
             </div>
 
           </div>
