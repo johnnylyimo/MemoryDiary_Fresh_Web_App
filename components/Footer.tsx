0e34a1222a691721555e8ea50a21628b03c30276
johnnylyimo <johnnylyimo@gmail.com>
2023-06-26 03:04:24 +0300

    Styled HTML h1 tag with TailwindCSS's class font-bold

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
       <section class='grid md:col-span-3 gap-6 text-center'>
         <div className="grid gap-y-6 md:flex md:justify-around">
           <div className="flex flex-col gap-2">
-            <h1 class="text-lg ">
+            <h1 class="text-lg font-bold">
 
             </h1>
 
