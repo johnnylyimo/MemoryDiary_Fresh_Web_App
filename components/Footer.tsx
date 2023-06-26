65ccbbcd53c2745c1612f114bfedfd9ca627b70d
johnnylyimo <johnnylyimo@gmail.com>
2023-06-26 03:00:02 +0300

    Styled HTML div tag with TailwindCSS's class md:justify-around

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
       </section>
 
       <section class='grid md:col-span-3 gap-6 text-center'>
-        <div className="grid gap-y-6 md:flex ">
+        <div className="grid gap-y-6 md:flex md:justify-around">
 
         </div>
 
