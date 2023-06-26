37dcb835789a6409ee35a71d5185c27aecd35172
johnnylyimo <johnnylyimo@gmail.com>
2023-06-26 03:28:30 +0300

    Defined text string in HTML anchor tag

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -28,7 +28,7 @@ export default function Footer() {
                 Twitter
               </a>
               <a href="https://www.linkedin.com/in/johnnylyimo" target="_blank" class="text-xl text-blue-500 font-bold">
-
+                LinkedIn
               </a>
             </div>
 
